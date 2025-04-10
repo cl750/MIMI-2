16c9f6c161c5ee9d59f1015ae2cc2703a1df5692
Clemens-Li <cl750@bu.edu>
2025-04-10 19:00:14 -0400

    Created executable file without any windows popping up, silently checks for flask, run.bat or main.py both work

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="../templates", static_folder="../static", static_url_path="/static")
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=False)
