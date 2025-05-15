07c9f4a69beb6eee742d1d4f94d43dce34b78a81
Clemens-Li <cl750@bu.edu>
2025-05-14 23:50:27 -0400

    Successfully pivoted to Webview and autopytoexe, struggled with directory troubles but bringing the main python file to the root of MIMI-2 seemed to fix the problem, v0.0 functional

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import psutil
+import webview
+
+app = Flask(__name__, template_folder="./templates", static_folder="./static")
+
+def kill(port):
+    for proc in psutil.process_iter():
+        try:
+            for conn in proc.net_connections('tcp'):
+                if conn.laddr.port == port:
+                    proc.terminate()
+                    proc.wait(timeout=5)
+                    return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+    return False
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+def on_closing():
+    kill(5000)
+
+if __name__ == "__main__":
+    # app.run(host="127.0.0.1", port=5000, debug=False)
+    kill(5000)
+    window = webview.create_window("DATEABASE", app)
+    window.events.closing += on_closing
+    webview.start()
+    
