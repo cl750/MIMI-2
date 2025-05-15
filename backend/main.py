7c4cbfa54be8b86b9a30aa4e536c7b8c24baffb5
Clemens-Li <cl750@bu.edu>
2025-05-14 22:12:22 -0400

    Pivoting to webview and autopytoexe, so far so good

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import webview
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static", static_url_path="/static")
 
@@ -6,5 +7,8 @@ app = Flask(__name__, template_folder="../templates", static_folder="../static",
 def home():
     return render_template("home.html")
 
+webview.create_window("DATEABASE", app)
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    # app.run(host="127.0.0.1", port=5000, debug=False)
+    webview.start()
