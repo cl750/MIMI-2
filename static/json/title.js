7734747d6faa2b0bb0f0c943f1406cbc827cb488
cl750 <clemensli123@gmail.com>
2025-05-21 00:25:13 -0400

    Implemented fade all, doesn't look too good

--- static/json/title.js
+++ static/json/title.js
@@ -16,6 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    async function fadeAll(elements) {
+        return new Promise((resolve) => {
+            elements.forEach((item) => { item.classList.add("fade-out"); });
+            resolve();
+        });
+    }
+
     const durations = {
         fadeInDuration: 1250,
         displayDuration: 1250,
@@ -25,12 +32,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const text1 = document.getElementById("title-text1");
     const background = document.getElementById("title-background");
     const logogroup = document.getElementById("title-logogroup");
+    const elements = [text1, background, logogroup];
 
 
     async function titleAnimations() {
         await animate(text1, true);
         await animate(background, false);
         await animate(logogroup, false);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await fadeAll(elements);
         // window.location.href = "/home";
     }
 
